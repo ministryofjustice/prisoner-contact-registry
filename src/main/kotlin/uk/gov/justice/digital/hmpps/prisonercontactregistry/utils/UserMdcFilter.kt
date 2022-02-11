425a98df89cbb0e12cd5d698e73665efb6892da8
Phil <philip.milne@digital.justice.gov.uk>
2022-02-11 14:19:40 +0000

    MDC filter for logging user id

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/utils/UserMdcFilter.kt
@@ -0,0 +1,53 @@
+package uk.gov.justice.digital.hmpps.prisonercontactregistry.utils
+
+import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.annotation.Order
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.FilterConfig
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+
+@Component
+@Order(1)
+class UserMdcFilter @Autowired constructor() : Filter {
+
+  override fun init(filterConfig: FilterConfig) {
+    // Initialise - no functionality
+  }
+
+  override fun destroy() {
+    // Destroy - no functionality
+  }
+
+  @Throws(IOException::class, ServletException::class)
+  override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+    val currentUsername = getUserName()
+    try {
+      currentUsername?.let { MDC.put(USER_ID_HEADER, currentUsername) }
+      chain.doFilter(request, response)
+    } finally {
+      currentUsername?.let { MDC.remove(USER_ID_HEADER) }
+    }
+  }
+
+  private fun getUserName(): String? {
+    return when (val principle = SecurityContextHolder.getContext().authentication.principal) {
+      is String -> { principle }
+      is UserDetails -> { principle.username }
+      is Map<*, *> -> { principle[USERNAME_ATTR] as String? }
+      else -> { null }
+    }
+  }
+
+  companion object {
+    const val USER_ID_HEADER = "userId"
+    const val USERNAME_ATTR = "username"
+  }
+}
