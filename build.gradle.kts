b5f0d334a131c79beedec466ecb05cf87834a7cb
Phil <philip.milne@digital.justice.gov.uk>
2022-07-15 13:08:48 +0100

    Upgrade to version 4.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.2"
   kotlin("jvm") version "1.7.0"
   kotlin("plugin.spring") version "1.7.0"
   idea
