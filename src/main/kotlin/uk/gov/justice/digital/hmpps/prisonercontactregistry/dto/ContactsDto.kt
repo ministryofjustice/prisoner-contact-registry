20b8dccc8bb358b2987176714931637150fa4bd7
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-06-07 13:10:59 +0100

    ktlint fixes.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/ContactsDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/ContactsDto.kt
@@ -1,5 +1,5 @@
 package uk.gov.justice.digital.hmpps.prisonercontactregistry.dto
 
 data class ContactsDto(
-  var offenderContacts: List<ContactDto> = listOf()
+  var offenderContacts: List<ContactDto> = listOf(),
 )
