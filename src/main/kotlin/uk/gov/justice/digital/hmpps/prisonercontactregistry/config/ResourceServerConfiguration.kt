c4ddc9818107753c0776b42a9f9b5a6f083f0c44
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-07-05 16:40:39 +0100

    VB-2414 - implementation of suggested workaround to cache jwks file. Also replaced deprecated EnableGlobalMethodSecurity with EnableMethodSecurity.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/ResourceServerConfiguration.kt
@@ -1,17 +1,23 @@
 package uk.gov.justice.digital.hmpps.prisonercontactregistry.config
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+@EnableCaching
+@EnableMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 class ResourceServerConfiguration {
 
   @Bean
@@ -39,4 +45,10 @@ class ResourceServerConfiguration {
     }
     return http.build()
   }
+
+  @Bean
+  fun locallyCachedJwtDecoder(
+    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String,
+    cacheManager: CacheManager,
+  ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).cache(cacheManager.getCache("jwks")).build()
 }
