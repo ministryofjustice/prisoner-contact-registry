336c3f64e5f76f3adc13b8a9f8cfa3aedef7d282
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2024-10-30 12:54:07 +0000

    Update dependencies (#156)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
-  kotlin("plugin.spring") version "2.0.20"
-  id("org.jetbrains.kotlin.plugin.noarg") version "2.0.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  kotlin("plugin.spring") version "2.0.21"
+  id("org.jetbrains.kotlin.plugin.noarg") version "2.0.21"
   id("org.jetbrains.kotlinx.kover") version "0.8.3"
 }
 
@@ -21,16 +21,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-cache")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.8.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.23")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
