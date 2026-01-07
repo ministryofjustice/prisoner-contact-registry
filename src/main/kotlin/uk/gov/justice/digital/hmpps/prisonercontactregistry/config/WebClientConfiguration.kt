d18236e83a9b87d5c2fcc58358454cbd44600dac
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-01-07 12:36:15 +0000

    Update creating client config to use caching similar to more recent projects e.g. visit-allocation-api.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/WebClientConfiguration.kt
@@ -3,56 +3,28 @@ package uk.gov.justice.digital.hmpps.prisonercontactregistry.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.codec.ClientCodecConfigurer
-import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
-import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
+import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
+import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
   @param:Value("\${prison.api.url}")
   private val prisonApiBaseUrl: String,
+
+  @param:Value("\${prison.api.timeout:10s}") private val apiTimeout: Duration,
+  @param:Value("\${prison.api.health-timeout:2s}") val healthTimeout: Duration,
 ) {
+  private val clientRegistrationId = "prison-api"
 
   @Bean
-  fun prisonApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
-    val oauth2Client = getOauth2Client(authorizedClientManager, "prison-api")
-    return getWebClient(prisonApiBaseUrl, oauth2Client)
-  }
+  fun prisonApiWebClient(
+    authorizedClientManager: OAuth2AuthorizedClientManager,
+    builder: WebClient.Builder,
+  ): WebClient = builder.authorisedWebClient(authorizedClientManager, registrationId = clientRegistrationId, url = prisonApiBaseUrl, apiTimeout)
 
   @Bean
-  fun prisonApiHealthWebClient(): WebClient = WebClient.builder().baseUrl(prisonApiBaseUrl).build()
-
-  @Bean
-  fun authorizedClientManager(
-    clientRegistrationRepository: ClientRegistrationRepository?,
-    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService?,
-  ): OAuth2AuthorizedClientManager? {
-    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
-    val authorizedClientManager =
-      AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService)
-    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
-    return authorizedClientManager
-  }
-
-  private fun getOauth2Client(authorizedClientManager: OAuth2AuthorizedClientManager, clientRegistrationId: String): ServletOAuth2AuthorizedClientExchangeFilterFunction {
-    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    oauth2Client.setDefaultClientRegistrationId(clientRegistrationId)
-    return oauth2Client
-  }
-
-  private fun getExchangeStrategies(): ExchangeStrategies = ExchangeStrategies.builder()
-    .codecs { configurer: ClientCodecConfigurer -> configurer.defaultCodecs().maxInMemorySize(-1) }
-    .build()
-
-  private fun getWebClient(baseUrl: String, oauth2Client: ServletOAuth2AuthorizedClientExchangeFilterFunction): WebClient = WebClient.builder()
-    .baseUrl(baseUrl)
-    .apply(oauth2Client.oauth2Configuration())
-    .exchangeStrategies(getExchangeStrategies())
-    .build()
+  fun prisonApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(prisonApiBaseUrl, healthTimeout)
 }
