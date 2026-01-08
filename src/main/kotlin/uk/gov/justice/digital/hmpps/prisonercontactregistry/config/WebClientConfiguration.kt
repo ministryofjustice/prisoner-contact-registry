7ecaf322eace0ef167ceed4238b82d5e029e274c
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-01-08 10:23:23 +0000

    Update authorizedClientManager to use GlobalPrincipalOAuth2AuthorizedClientService as suggested by Auth team

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/config/WebClientConfiguration.kt
@@ -3,11 +3,17 @@ package uk.gov.justice.digital.hmpps.prisonercontactregistry.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
+import uk.gov.justice.hmpps.kotlin.auth.service.GlobalPrincipalOAuth2AuthorizedClientService
 import java.time.Duration
+
+private const val CLIENT_REGISTRATION_ID = "prison-api"
 
 @Configuration
 class WebClientConfiguration(
@@ -17,14 +23,36 @@ class WebClientConfiguration(
   @param:Value("\${prison.api.timeout:10s}") private val apiTimeout: Duration,
   @param:Value("\${prison.api.health-timeout:2s}") val healthTimeout: Duration,
 ) {
-  private val clientRegistrationId = "prison-api"
-
   @Bean
   fun prisonApiWebClient(
     authorizedClientManager: OAuth2AuthorizedClientManager,
     builder: WebClient.Builder,
-  ): WebClient = builder.authorisedWebClient(authorizedClientManager, registrationId = clientRegistrationId, url = prisonApiBaseUrl, apiTimeout)
+  ): WebClient = getWebClient(prisonApiBaseUrl, authorizedClientManager, builder)
 
   @Bean
   fun prisonApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(prisonApiBaseUrl, healthTimeout)
+
+  @Bean
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+  ): OAuth2AuthorizedClientManager {
+    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+    val authorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
+      clientRegistrationRepository,
+      GlobalPrincipalOAuth2AuthorizedClientService(clientRegistrationRepository),
+    )
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+    return authorizedClientManager
+  }
+
+  private fun getWebClient(
+    url: String,
+    authorizedClientManager: OAuth2AuthorizedClientManager,
+    builder: WebClient.Builder,
+  ): WebClient = builder.authorisedWebClient(
+    authorizedClientManager = authorizedClientManager,
+    url = url,
+    registrationId = CLIENT_REGISTRATION_ID,
+    timeout = apiTimeout,
+  )
 }
