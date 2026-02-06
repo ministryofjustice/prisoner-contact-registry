cd0a0696763d3888523c5234717a1a2b0e55b623
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-02-06 12:21:39 +0000

    VB-5969 - Replace prison-api client with personal-relationships-api client
    Condense some of the DTOs for prisoner-relationship-api restrictions as most are not
    used outside of the top level class PrisonerContactRestrictionsResponseDto

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/PrisonerContactIdsRequestDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/PrisonerContactIdsRequestDto.kt
@@ -3,6 +3,6 @@ package uk.gov.justice.digital.hmpps.prisonercontactregistry.dto.personal.relati
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class PrisonerContactIdsRequestDto(
-  @param:Schema(description = "List of prisoner contact identifiers", example = "[123456, 789012]", required = true)
+  @param:Schema(description = "List of prisoner contact relationship identifiers [NOT contactId]", example = "[123456, 789012]", required = true)
   val prisonerContactIds: List<Long>,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/PrisonerContactRestrictionsResponseDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/PrisonerContactRestrictionsResponseDto.kt
@@ -1,5 +1,37 @@
 package uk.gov.justice.digital.hmpps.prisonercontactregistry.dto.personal.relationships
 
+import java.time.LocalDate
+
 data class PrisonerContactRestrictionsResponseDto(
   val prisonerContactRestrictions: List<PrisonerContactRestrictionsDto>,
+)
+
+data class PrisonerContactRestrictionsDto(
+  val prisonerContactId: Long,
+  val prisonerContactRestrictions: List<PrisonerContactRestrictionDto>,
+  val globalContactRestrictions: List<GlobalContactRestrictionDto>,
+)
+
+data class PrisonerContactRestrictionDto(
+  val prisonerContactRestrictionId: Long,
+  val prisonerContactId: Long,
+  val contactId: Long,
+  val prisonerNumber: String,
+  val restrictionType: String,
+  val restrictionTypeDescription: String,
+  val startDate: LocalDate,
+  val expiryDate: LocalDate?,
+  val comments: String?,
+  val enteredByDisplayName: String,
+)
+
+data class GlobalContactRestrictionDto(
+  val contactRestrictionId: Long,
+  val contactId: Long,
+  val restrictionType: String,
+  val restrictionTypeDescription: String,
+  val startDate: LocalDate,
+  val expiryDate: LocalDate?,
+  val comments: String?,
+  val enteredByDisplayName: String,
 )
