42ca342a6f79908b26086c8ca25bb4fd12e15701
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-04-22 13:24:28 +0100

    update swagger

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/ContactLinkedPrisonerDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/dto/personal/relationships/ContactLinkedPrisonerDto.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.prisonercontactregistry.dto.personal.relati
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "A contact (no prisoner relationship)")
+@Schema(description = "A linked prisoner of contact")
 data class ContactLinkedPrisonerDto(
   @param:Schema(description = "Prisoner number (NOMS ID)", example = "A1234BC")
   val prisonerNumber: String,
