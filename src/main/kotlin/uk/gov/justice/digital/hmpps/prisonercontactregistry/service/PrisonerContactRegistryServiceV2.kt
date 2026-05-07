8770d22361c510eb961be4b79729dd6439eb9445
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-05-07 14:57:38 +0100

    rename convert method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/service/PrisonerContactRegistryServiceV2.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonercontactregistry/service/PrisonerContactRegistryServiceV2.kt
@@ -151,7 +151,7 @@ class PrisonerContactRegistryServiceV2(private val personalRelationshipsApiClien
       null
     }
 
-    val prisonerContact = convertToContactDto(listOf(contact), allPrisonerContactRestrictions).firstOrNull()
+    val prisonerContact = convertToPrisonerContactDto(listOf(contact), allPrisonerContactRestrictions).firstOrNull()
     if (prisonerContact == null) {
       throw VisitorNotFoundException(message = "Contact with id $contactId not found for prisoner $prisonerId, for relationship (prisonerContactId) $relationshipId")
     }
@@ -183,7 +183,7 @@ class PrisonerContactRegistryServiceV2(private val personalRelationshipsApiClien
       null
     }
 
-    return convertToContactDto(prisonerContacts, allPrisonerContactRestrictions)
+    return convertToPrisonerContactDto(prisonerContacts, allPrisonerContactRestrictions)
   }
 
   /**
@@ -202,7 +202,7 @@ class PrisonerContactRegistryServiceV2(private val personalRelationshipsApiClien
    * Returns:
    * - A ContactDto list [to keep the exact structure as the previous client prison-api had]
    */
-  private fun convertToContactDto(
+  private fun convertToPrisonerContactDto(
     prisonerContactsList: List<PersonalRelationshipsPrisonerContactDto>,
     prisonerContactRestrictions: PrisonerContactRestrictionsResponseDto?,
   ): List<PrisonerContactDto> {
